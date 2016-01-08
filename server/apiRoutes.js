10eecf75f00487c8fd705779eb3f191eee8dd1ea
Mad Bernard <madeline.a.bernard@gmail.com>
2016-01-08 10:31:35 -0800

    Refactor whichController.js, add comments, add variable routes
    Refactor /api/which/:id/judge route, judgeWhich function, getWhichByID function, and add clear comments documenting the interfaces of whichController.js
    Update documentation comments in apiRoutes.js
    Add route /api/tag/:tagName GET
    Add route /api/which/:id/tag POST

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,19 +1,38 @@
 var whichController = require('./which/whichController.js');
 
-// this comes from middleware.js
 module.exports = function (apiRouter) {
 
-  // reaches into the database and comes back with a which object
-  // FOR DEVELOPMENT is getNewestWhich, otherwise getOldestWhich
+  /*     Routes beginning with /api/which
+
+       See documentation at corresponding
+       function in whichController.js
+  */
   apiRouter.get('/which', whichController.getNewestWhich);
-  // reaches into the database and places a new which object in it
   apiRouter.post('/which', whichController.createWhich);
+  apiRouter.post('/which/:whichID/judge', whichController.judgeWhich);
+  apiRouter.post('/which/:whichID/tag',   whichController.tagWhich);
+
+
 
   // TODO: implement getting which by id
-  // apiRouter.get('/which/:id', function () {});
-
-  // updates the database with the results of a vote
-  apiRouter.post('/which/:id/judge', function () {});
+  // apiRouter.get('/which/:whichID', function () {});
 
 
+  apiRouter.get('/tag/:tagName', whichController.getWhichesByTag);
+
+
+
+  /*   For all dynamic routes containing a :whichID or :tagName,
+       before passing the request along to its handler
+       create a property on req.body of the same name,
+       containing the dynamic value
+  */
+  apiRouter.param('tagName', function(req, res, next, tagName){
+    req.body.tagName = tagName;
+    next();
+  });
+  apiRouter.param('whichID', function(req, res, next, whichID){
+    req.body.whichID = whichID;
+    next();
+  });
 };
